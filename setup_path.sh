b65940ed3e7d86fcca1baed88186c9035031b6e5
Himanshu Neema <himanshun.iitkgp@gmail.com>
2016-08-17 14:10:52 +0530

    ANDROID_HOME

--- setup_path.sh
+++ setup_path.sh
@@ -1,6 +1,7 @@
 #Android platform tools
 echo 'export PATH=$PATH:~/Library/Android/sdk/platform-tools' >> ~/.zshrc
 echo 'export PATH=$PATH:~/Library/Android/sdk/tools' >> ~/.zshrc
+echo 'export ANDROID_HOME=~/Library/Android/sdk' >> ~/.zshrc
 
 #GO LANG
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
